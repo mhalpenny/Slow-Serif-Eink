368a5b3b20a23b5c88f5cdc6162a01d1a4c668dd
mhalpenny <matt.halpenny@gmail.com>
2021-04-14 13:59:24 -0400

    new bin file

---
+++ target/2.0.1/photon/obj/src/module_info.o.d
@@ -0,0 +1,48 @@
+/home/matthew/Developer/eink-project/target/2.0.1/photon//obj/src/module_info.o: \
+ src/module_info.c ../../../dynalib/inc/module_info.inc \
+ ../../../dynalib/inc/module_info.h ../../../services/inc/static_assert.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/assert.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h
+
+../../../dynalib/inc/module_info.inc:
+
+../../../dynalib/inc/module_info.h:
+
+../../../services/inc/static_assert.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/assert.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h:
---
+++ target/2.0.1/photon/obj/src/newlib_stubs.o.d
@@ -0,0 +1,149 @@
+/home/matthew/Developer/eink-project/target/2.0.1/photon//obj/src/newlib_stubs.o: \
+ src/newlib_stubs.cpp \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/types.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/cdefs.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_types.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_types.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/lock.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/endian.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_endian.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/select.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_sigset.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timeval.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/timespec.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timespec.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/sched.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/types.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/stdlib.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/cstdlib \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/c++config.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/os_defines.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/cpu_defines.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdlib.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/reent.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/stdlib.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/alloca.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/xlocale.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/bits/std_abs.h \
+ ../../../services/inc/debug.h ../../../services/inc/service_debug.h \
+ ../../../services/inc/logging.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/string.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/strings.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/string.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h \
+ /home/matthew/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ ../../../services/inc/panic.h ../../../services/inc/panic_codes.h \
+ ../../../services/inc/config.h ../../../services/inc/preprocessor.h
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/types.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/cdefs.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_types.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_types.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/lock.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/endian.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_endian.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/select.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_sigset.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timeval.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/timespec.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timespec.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/sched.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/types.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/stdlib.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/cstdlib:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/c++config.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/os_defines.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/cpu_defines.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdlib.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/reent.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/stdlib.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/alloca.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/xlocale.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/bits/std_abs.h:
+
+../../../services/inc/debug.h:
+
+../../../services/inc/service_debug.h:
+
+../../../services/inc/logging.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/string.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/strings.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/string.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h:
+
+/home/matthew/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h:
+
+../../../services/inc/panic.h:
+
+../../../services/inc/panic_codes.h:
+
+../../../services/inc/config.h:
+
+../../../services/inc/preprocessor.h:
